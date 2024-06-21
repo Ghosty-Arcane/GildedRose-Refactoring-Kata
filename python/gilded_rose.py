543b514d1916e5f436698208fdf9500f190cb882
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-21 15:44:24 +1000

    Deleted stuff, added types

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -3,7 +3,7 @@
 class GildedRose(object):
 
     def __init__(self, items):
-        self.items = items
+        self.items: list[Item] = items
 
     def update_quality(self):
         for item in self.items:
@@ -38,9 +38,9 @@ class GildedRose(object):
 
 class Item:
     def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
+        self.name: str = name
+        self.sell_in: int = sell_in
+        self.quality: int = quality
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
