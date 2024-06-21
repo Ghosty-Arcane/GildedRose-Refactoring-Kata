37f38cb381e855eafe2bcc0ad8d3353bdaf0bff3
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-21 17:09:16 +1000

    Final refactor

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
+from enum import Enum
 
-BRIE = 'Aged Brie'
-BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert'
-SULFURAS = 'Sulfuras, Hand of Ragnaros'
-CONJURED = 'Conjured Mana Cake'
+class Special(Enum):
+    BRIE = 'Aged Brie'
+    BACKSTAGE = 'Backstage passes to a TAFKAL80ETC concert'
+    SULFURAS = 'Sulfuras, Hand of Ragnaros'
+    CONJURED = 'Conjured Mana Cake'
+
 
 class GildedRose(object):
 
@@ -13,28 +16,29 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name == SULFURAS:
+            if Special.SULFURAS.value in item.name:
                 continue
             # Handles sell-in drop
             item.sell_in = item.sell_in - 1
 
             # Handles quality drop
-            qualityChange = -1
-            if item.sell_in < 0:
-                qualityChange = qualityChange *2
-            if item.name == BACKSTAGE:
+            if Special.BACKSTAGE.value in item.name:
                 if item.sell_in < 0:
-                    qualityChange = -item.quality
+                    item.quality = 0
+                    continue
                 elif item.sell_in < 6:
                     qualityChange = 3
                 elif item.sell_in < 11:
                     qualityChange = 2
                 else:
                     qualityChange = 1
-            elif item.name == BRIE:
-                qualityChange = -qualityChange
-            elif item.name == CONJURED:
-                qualityChange = qualityChange *2
+            elif Special.BRIE.value in item.name:
+                qualityChange = 2 if item.sell_in < 0 else 1
+            elif Special.CONJURED.value in item.name:
+                qualityChange = -4 if item.sell_in < 0 else -2
+            else:
+                qualityChange = -2 if item.sell_in < 0 else -1
+
             self._QualityChanger(item, qualityChange)
             
 
