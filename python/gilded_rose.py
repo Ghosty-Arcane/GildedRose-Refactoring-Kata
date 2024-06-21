59f9bafb1a7f39591e1aee0808d02f766796cde7
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-21 16:28:35 +1000

    Inverted check for Brie and Backstage

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -13,11 +13,12 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
+            # Handles sell-in drop
+            if item.name != SULFURAS:
+                item.sell_in = item.sell_in - 1
+
             # Handles quality drop
-            if item.name != BRIE and item.name != BACKSTAGE:
-                if item.quality > 0:
-                    self._QualityChanger(item, -1)
-            else:
+            if item.name == BRIE or item.name == BACKSTAGE:
                 if item.quality < 50:
                     self._QualityChanger(item, 1)
                     if item.name == BACKSTAGE:
@@ -27,10 +28,10 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 self._QualityChanger(item, 1)
+            else:
+                if item.quality > 0:
+                    self._QualityChanger(item, -1)
 
-            # Handles sell-in drop
-            if item.name != SULFURAS:
-                item.sell_in = item.sell_in - 1
 
             # Handles expiry quality drop
             if item.sell_in < 0:
@@ -48,6 +49,10 @@ class GildedRose(object):
         if item.name == SULFURAS:
             return
         item.quality = item.quality + change
+        if item.quality < 0:
+            item.quality == 0
+        elif item.quality > 50:
+            item.quality == 50
         return
 
 class Item:
