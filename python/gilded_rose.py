79435776d88d24a993034a0ce4a664c94ff4b956
Ghosty-Arcane <ghosty.arcane@gmail.com>
2024-06-21 16:45:54 +1000

    Used quality change variable

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -18,41 +18,31 @@ class GildedRose(object):
                 item.sell_in = item.sell_in - 1
 
             # Handles quality drop
-            if item.name == BRIE or item.name == BACKSTAGE:
-                if item.quality < 50:
-                    self._QualityChanger(item, 1)
-                    if item.name == BACKSTAGE:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                self._QualityChanger(item, 1)
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                self._QualityChanger(item, 1)
-            else:
-                if item.quality > 0:
-                    self._QualityChanger(item, -1)
-
-
-            # Handles expiry quality drop
+            qualityChange = -1
             if item.sell_in < 0:
-                if item.name != BRIE:
-                    if item.name != BACKSTAGE:
-                        if item.quality > 0:
-                            self._QualityChanger(item, -1)
-                    else:
-                        item.quality = 0
+                qualityChange = qualityChange *2
+            if item.name == BACKSTAGE:
+                if item.sell_in < 0:
+                    qualityChange = -item.quality
+                elif item.sell_in < 6:
+                    qualityChange = 3
+                elif item.sell_in < 11:
+                    qualityChange = 2
                 else:
-                    if item.quality < 50:
-                        self._QualityChanger(item, 1)
+                    qualityChange = 1
+            elif item.name == BRIE:
+                qualityChange = -qualityChange
+            elif item.name == CONJURED:
+                qualityChange = qualityChange *2
+            elif item.name == SULFURAS:
+                return
+            self._QualityChanger(item, qualityChange)
+            
 
     def _QualityChanger(self, item: Item, change: int) ->None:
         if item.name == SULFURAS:
             return
-        item.quality = item.quality + change
-        if item.quality < 0:
-            item.quality == 0
-        elif item.quality > 50:
-            item.quality == 50
+        item.quality = max(0, min(50, item.quality + change))
         return
 
 class Item:
